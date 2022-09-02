72dcfcc8923f46f69f6f1a594a18a4a65114543d
Dou <dou.du.epfl@gmail.com>
2022-09-02 15:06:01 +0800

    fix the bug

--- test/test.py
+++ test/test.py
@@ -33,7 +33,7 @@ class TestTest01():
     self.driver.find_element(By.LINK_TEXT, "Avoided Crossing in 1D Asymmetric Quantum Well").click()
     time.sleep(5)
     self.driver.execute_script("window.scrollTo(0, 1000)")
-    self.driver.find_element(By.LINK_TEXT, "Probability density").click()
+    self.driver.find_element(By.CSS_SELECTOR, "label:nth-child(2) > input").click()
     self.driver.save_screenshot("asymmetricwell.png")
 
 test = TestTest01()
