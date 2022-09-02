4a4a6af1b73548ceca868abd739750188125330f
Dou <dou.du.epfl@gmail.com>
2022-09-02 16:24:30 +0800

    10s to get change

--- test/test.py
+++ test/test.py
@@ -36,7 +36,7 @@ class TestTest01():
     time.sleep(5)
     self.driver.execute_script("window.scrollTo(0, 1000)")
     self.driver.find_element(By.CSS_SELECTOR, "label:nth-child(2) > input").click()
-    time.sleep(3)
+    time.sleep(10)
     self.driver.save_screenshot("asymmetricwell.png")
 
 test = TestTest01()
